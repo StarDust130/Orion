8016206f3e171edfc3e4fe0020795eb06e0c983d
StarDust130 <csyadav0513@gmail.com>
2026-07-25 21:07:29 +0530

    add conv his

--- orion-backend/app/prompts/builder.py
+++ orion-backend/app/prompts/builder.py
@@ -4,12 +4,17 @@ from groq.types.chat import ChatCompletionMessageParam
 
 class PromptBuilder:
     @staticmethod
-    def build_chat_prompt(message: str) -> list[ChatCompletionMessageParam]:
+    def build_chat_prompt(
+        history: list[ChatCompletionMessageParam],
+        message: str,
+    ) -> list[ChatCompletionMessageParam]:
+
         return [
             {
                 "role": "system",
                 "content": CHAT_SYSTEM_PROMPT,
             },
+            *history,
             {
                 "role": "user",
                 "content": message,
